b075dc517fff34edf111b4b37c498cdb587693ad
Marina Samokhina <m.g.samokhina@gmail.com>
2025-06-06 23:35:55 +0300

    Initial commit

---
+++ BalancePlus/BalancePlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalancePlusApp.swift
+//  BalancePlus
+//
+//  Created by Самохина Марина Германовна on 06.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct BalancePlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BalancePlus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BalancePlus
+//
+//  Created by Самохина Марина Германовна on 06.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
